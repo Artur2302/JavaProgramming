5f50352a9ab1f484c768891b0f2b6deb8f5a38b2
arthurstepanyan <artur23.stepanyan@gmail.com>
2022-03-12 17:08:32 +0400

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        switch (a){
+            case 2:
+                System.out.println("two");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            default:
+                System.out.println("default");
+        }
+    }
+}
